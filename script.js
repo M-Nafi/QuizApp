6d66939dc5421db1ff1ceb06ac5eab9bf247d8cd
M-Nafi <nafi-m@gmx.de>
2023-08-18 12:14:11 +0200

    update 2

--- script.js
+++ script.js
@@ -48,14 +48,59 @@ function init() {
 }
 
 function showQuestion() {
-  let question = questions[currentQuestion];
-  document.getElementById("questiontext").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answer_1"];
-  document.getElementById("answer_2").innerHTML = question["answer_2"];
-  document.getElementById("answer_3").innerHTML = question["answer_3"];
-  document.getElementById("answer_4").innerHTML = question["answer_4"];
+  if (currentQuestion >= questions.length) {// ">=" heisst = grösser oder gleich
+    // Todo: show end screen
+  } else {
+    let question = questions[currentQuestion];
+    document.getElementById("question-number").innerHTML = currentQuestion + 1; // für die zahl unten auf der seite
+    document.getElementById("questiontext").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+  }
 }
 
 function answer(selection) {
   let question = questions[currentQuestion];
+  // console.log("Selected answer is", selection); // herausfinden, welche variable drin ist
+  let selectedQuestionNumber = selection.slice(-1);
+  // console.log("selectedQuestionNumber is", selectedQuestionNumber);
+  // console.log("Current question is", question["right_answer"]);
+
+  let idOfRightAnswer = `answer_${question["right_answer"]}`; //sicherstellung, dass die answeres gewählt werden von jedem array
+
+  if (selectedQuestionNumber == question["right_answer"]) {
+    // console.log("Richtige antwort!");
+    document.getElementById(selection).parentNode.classList.add("bg-success"); // parentNode gibt css klasse dem übergeordneten container
+  } else {
+    // console.log("Falsche Antwort!");
+    document.getElementById(selection).parentNode.classList.add("bg-danger");
+    document
+      .getElementById(idOfRightAnswer)
+      .parentNode.classList.add("bg-success");
+  }
+  document.getElementById("next-button").disabled = false; // funktion für button anklickbar nach auswahl der antwort
+}
+
+function nextQuestion() {
+  currentQuestion++; // zb von 0 auf 1
+
+  document.getElementById("next-button").disabled = true;
+  resetAnswerButtons();
+  showQuestion();
+}
+
+function resetAnswerButtons() {
+  document.getElementById("answer_1").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_1").parentNode.classList.remove("bg-success");
+
+  document.getElementById("answer_2").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_2").parentNode.classList.remove("bg-success");
+
+  document.getElementById("answer_3").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_3").parentNode.classList.remove("bg-success");
+
+  document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_4").parentNode.classList.remove("bg-success");
 }
